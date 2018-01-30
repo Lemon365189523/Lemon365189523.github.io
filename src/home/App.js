b6990f05e69139282544967cd42709d9476da3aa
Lemon <365189523@qq.com>
2018-01-30 21:50:39 +0800

    home 组件做主页

--- src/home/App.js
+++ src/home/App.js
@@ -1,1 +1,15 @@
 
+
+import React from "react";
+import Index from "./index/index"
+
+export default class App extends React.Component {
+    render (){
+        return (
+            <div>
+                <Index/>
+            </div>
+
+        )
+    }
+}
