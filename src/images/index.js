148a3515adb36a7520a00819b2e8a0951e37f92c
fan <365189523@qq.com>
2024-07-02 15:27:21 +0800

    更新项目

--- src/images/index.js
+++
@@ -1,7 +0,0 @@
-export default  {
-    inch1: require('./Inch1.jpg'),
-    inch2: require('./Inch2.jpg'),
-    inch3: require('./Inch3.jpg'),
-    inch4: require('./Inch4.jpg'),
-    inch5: require('./Inch5.jpg')
-}
